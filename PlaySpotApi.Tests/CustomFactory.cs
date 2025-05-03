c9ff0d995647a03b00e93379636592a01ca3111f
Sean Clayton <sean.clayto@gmail.com>
2025-05-03 02:18:33 -0700

    setup InMemoryDb for endpoint testing

---
+++ PlaySpotApi.Tests/CustomFactory.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+using PlaySpotApi.Data;
+
+namespace PlaySpotApi.Tests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.Where(d => d.ServiceType == typeof(IDbContextOptionsConfiguration<PlaySpotDbContext>))
+                    .ToList()
+                    .ForEach(d => 
+                    {
+                        services.Remove(d);
+                    });
+
+                services.AddDbContext<PlaySpotDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TestDb");
+                });
+
+                var serviceProvider = services.BuildServiceProvider();
+                using var scope = serviceProvider.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<PlaySpotDbContext>();
+                db.Database.EnsureCreated();
+
+                db.SaveChanges();
+            });
+        }
+    }
+}
--- PlaySpotApi/Routes/LocationRoutes.cs
+++ PlaySpotApi/Routes/LocationRoutes.cs
@@ -1,6 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 
 using PlaySpotApi.Data;
